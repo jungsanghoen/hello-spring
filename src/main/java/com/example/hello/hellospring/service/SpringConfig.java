2be7ef2105b468653f1df75f338bc24bd4c992d0
jungsangheon <jusahe@hotmail.com>
2022-08-20 13:11:43 +0900

    1. JPA 사용하도록 commit

--- src/main/java/com/example/hello/hellospring/service/SpringConfig.java
+++ src/main/java/com/example/hello/hellospring/service/SpringConfig.java
@@ -1,9 +1,13 @@
 package com.example.hello.hellospring.service;
 
+import com.example.hello.hellospring.repository.JpaMemberRepository;
 import com.example.hello.hellospring.repository.MemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.persistence.EntityManager;
+import javax.sql.DataSource;
 
 @Configuration
 
@@ -15,48 +19,48 @@ import org.springframework.context.annotation.Configuration;
 
 public class SpringConfig {
     //JDBC 용 dataSource
-    /*
+    //JPA 사용을 위해서도 필요
     private DataSource dataSource;
 
     @Autowired
     public SpringConfig(DataSource dataSource) {
         this.dataSource = dataSource;
     }
-    */
+
 
     //JPA를 위한 선언부
-    /*
+
     private EntityManager em;
 
     @Autowired
     public SpringConfig(EntityManager em) {
         this.em = em;
     }
-    */
 
-    private final MemberRepository memberRepository;
 
+    //private final MemberRepository memberRepository;
+    /*
     @Autowired
     public SpringConfig(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
-
+    */
     @Bean
     //스프링 빈에 등록
     public MemberService memberService()
     {
 
-        //return new MemberService(memberRepository());
+        return new MemberService(memberRepository());
         //리포지토리 등록용
-        return new MemberService(memberRepository);
+        //return new MemberService(memberRepository);
     }
-    /*
+
     @Bean
     //리포지토리 등록
     public MemberRepository memberRepository()
     {
-        //return new JpaMemberRepository(em);
+        return new JpaMemberRepository(em);
     }
 
-     */
+
 }
